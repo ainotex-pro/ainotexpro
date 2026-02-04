50d899a21c29183e70edb80dc848de8330801722
Ai NoteX Pro <ainotexpro@gmail.com>
2026-02-04 16:04:41 +0900

    Add files via upload

--- serviceworkerjs/service-worker.js
+++ serviceworkerjs/service-worker.js
@@ -1,14 +1,29 @@
-const CACHE_NAME = "ainotexpro-cache-v1";
+const CACHE_NAME = "ainotexpro-cache-v2";
 const ASSETS = [
   "/",
-  "/hello.html",
+"/index.html",   // 
+  "/login.html",   // 
+  "/main.html",    // 
   "/style.css",
   "/app.js",
   "/manifest.json",
   "/icons/icon-152.png",
   "/icons/icon-192.png",
   "/icons/icon-512.png",
-  "/icons/icon-1024.png",
+  "/icons/icon-1024.png",<div class="piano-wrapper">
+        <div class="piano-box">
+            <div class="label">🏛️ GRAND MODE (88)</div>
+            <div class="keys-container" id="pcPiano"></div>
+        </div>
+    </div>
+
+    <div class="piano-wrapper">
+        <div class="piano-box">
+            <div class="label">📱 MOBILE MODE (10)</div>
+            <div class="keys-container" id="mbPiano"></div>
+        </div>
+    </div>
+
   "/icons/favicon-32.png"
 ];
 
